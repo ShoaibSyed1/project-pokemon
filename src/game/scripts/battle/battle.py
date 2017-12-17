cf05325517eba255cb9916d18fa51567f8d72213
Shoaib <560117@pdsb.net>
2017-12-16 23:07:57 -0500

    More work on battle ui

--- src/game/scripts/battle/battle.py
+++ src/game/scripts/battle/battle.py
@@ -9,6 +9,9 @@ class BattleController(Script):
         self.state = BattleState.WAITING
 
         self.attack_buttons = [None, None, None, None]
+        self.item_button = None
+        self.poke_button = None
+        self.run_button = None
 
     def start(self):
         from game.components import ScriptComponent
@@ -18,17 +21,46 @@ class BattleController(Script):
         self.pokemon_back = EntityLoader.load("battle/back", self.world,
                               {'sprite': "pokemon/battle/" + self.battle_info['front_info']['pokemon']})
 
+        EntityLoader.load("battle/ui/panel", self.world)
+
         for i in range(0, 4):
-            self.attack_buttons[i] = EntityLoader.load("battle/ui/button_attack", self.world, {
+            self.attack_buttons[i] = EntityLoader.load("battle/ui/button_left", self.world, {
                 "element": {
-                    "pos": [0, i * 80]
+                    "pos": [0, i * 80 + 8]
                 },
                 "script": {
-                    "args": "Attack"
+                    "args": "Attack #" + str(i)
                 }
             })
 
             self.world.component_for_entity(self.attack_buttons[i], ScriptComponent).script.set_cb_enter(self.attack_hover, i)
+        
+        self.item_button = EntityLoader.load("battle/ui/button_left", self.world, {
+            "element": {
+                "pos": [0, 332]
+            },
+            "script": {
+                "args": "Item"
+            }
+        })
+
+        self.poke_button = EntityLoader.load("battle/ui/button_left", self.world, {
+            "element": {
+                "pos": [0, 412]
+            },
+            "script": {
+                "args": "Pokemon"
+            }
+        })
+
+        self.run_button = EntityLoader.load("battle/ui/button_left", self.world, {
+            "element": {
+                "pos": [0, 492]
+            },
+            "script": {
+                "args": "Run"
+            }
+        })
     
     def update(self, delta):
         pass
